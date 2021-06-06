214d2d7cf656549bc7496bf8cc193af425f96b77
MyUsername <simo.3190@gmail.com>
2021-06-06 11:27:42 +0200

    tutti gli snack

---
+++ snack5/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+
+
+<?php
+
+$stringa = "Restate fermi, restate fermi. Figli di Gondor, di Rohan, fratelli miei! Vedo nei vostri occhi la stessa paura che potrebbe afferrare il mio cuore. Ci sarà un giorno in cui il coraggio degli uomini cederà, in cui abbandoneremo gli amici e spezzeremo ogni legame di fratellanza, ma non è questo il giorno! Ci sarà l’ora dei lupi e degli scudi frantumati quando l’era degli uomini arriverà al crollo, ma non è questo il giorno! Quest’oggi combattiamo! Per tutto ciò che ritenete caro su questa bella Terra, vi invito a resistere! Uomini dell’ovest!";
+
+
+var_dump(explode('!', $stringa));
+?>
+
+</body>
+</html>
+
+
+
+
+    
+
